7d39bbcffd9407d8b13fbb39ac16b51f648762fc
Adele Landers <adelethefish@gmail.com>
2018-01-04 11:25:02 -0800

    adding FlaskForm validation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, redirect, render_template, url_for, request
+from forms import EmailForm
 import os
 
 app = Flask(__name__)
@@ -7,9 +8,15 @@ app = Flask(__name__)
 def root():
     return redirect(url_for('index'))
 
-@app.route('/data')
+@app.route('/data', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = EmailForm(request.form, csrf_enabled=False)
+    if request.method == 'POST':
+        if form.validate():
+            flash('Your email message has been sent!')
+            return redirect(url_for('index'))
+
+    return render_template('index.html', form=form)
 
 if os.environ.get('ENV') == 'production':
     debug = False
